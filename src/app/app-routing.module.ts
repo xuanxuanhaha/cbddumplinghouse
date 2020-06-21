2a799da53530420e377b05dd50c3cec02cb817ab
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-06-21 13:48:04 +1000

    add google api with circle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {FormsModule} from '@angular/forms';
+import {GoogleMapComponentComponent} from './google-map-component/google-map-component.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import {RouterModule} from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { GoogleMapComponentComponent } from './google-map-component/google-map-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    GoogleMapComponentComponent
   ],
   imports: [
     BrowserModule,
